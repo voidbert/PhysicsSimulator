bff65fe0a6d74ac98d7b5bbe23c65eb59aabe0e4
Voidbert <50591320+voidbert@users.noreply.github.com>
2021-10-05 12:08:49 +0100

    Body class

---
+++ ts/Body.ts
@@ -0,0 +1,26 @@
+class Body {
+	public mass: number; //The mass of the body
+	public r: Vec2; //The position of the body
+	public v: Vec2; //The velocity of the body
+	public forces: Vec2[]; //The list of forces applied to the body
+
+	constructor(mass: Number, r: Vec2 = new Vec2()) {
+		this.r = r;
+		this.v = new Vec2(0, 0);
+		this.forces = [];
+	}
+
+	//Advances the simulation after some time (dt IN SECONDS) has passed
+	step(dt: number) {
+		//Calculate the resultant of the forces and the acceleration
+		let Fr: Vec2 = new Vec2();
+		for (let i: number = 0; i < this.forces.length; ++i) {
+			Fr.add(this.forces[i]);
+		}
+		let a = Fr.scale(1 / this.mass);
+		
+		//Add the acceleration to the the velocity and the velocity to the position
+		this.v.add(a.scale(dt));
+		this.r.add(this.v.scale(dt));
+	}
+}
