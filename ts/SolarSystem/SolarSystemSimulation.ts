512e863065c3390e2b73ba0e84d6b36161316f19
Voidbert <50591320+voidbert@users.noreply.github.com>
2022-05-12 22:31:43 +0100

    Improved planet data

--- ts/SolarSystem/SolarSystemSimulation.ts
+++ ts/SolarSystem/SolarSystemSimulation.ts
@@ -11,20 +11,21 @@ class SolarSystemSimulation {
 	static camera: Camera = new Camera(new Vec2(), new Vec2(3e-9, 3e-9));
 
 	//Creates the body manager and the planets
+	//Source: https://nssdc.gsfc.nasa.gov/planetary/factsheet/
 	private static generateSolarSystem() {
 		let bodies: Body[] = [
 			new Body(1.988e30, PLANET_GEOMETRY, new Vec2(0, 0)), //Sun
-			new Body(3.301e23, PLANET_GEOMETRY, new Vec2(0, 5.791e10)), //Mercury
-			new Body(4.868e24, PLANET_GEOMETRY, new Vec2(0, 1.082e11)), //Venus
-			new Body(5.972e24, PLANET_GEOMETRY, new Vec2(0, 1.495e11)), //Earth
-			new Body(6.417e23, PLANET_GEOMETRY, new Vec2(0, 2.279e11)), //Mars
+			new Body(3.30e23,  PLANET_GEOMETRY, new Vec2(0, 5.790e10)), //Mercury
+			new Body(4.87e24,  PLANET_GEOMETRY, new Vec2(0, 1.082e11)), //Venus
+			new Body(5.97e24,  PLANET_GEOMETRY, new Vec2(0, 1.496e11)), //Earth
+			new Body(6.42e23,  PLANET_GEOMETRY, new Vec2(0, 2.280e11)), //Mars
 			new Body(1.898e27, PLANET_GEOMETRY, new Vec2(0, 7.785e11)), //Jupiter
-			new Body(5.683e26, PLANET_GEOMETRY, new Vec2(0, 1.434e12)), //Saturn
-			new Body(8.681e25, PLANET_GEOMETRY, new Vec2(0, 2.871e12)), //Uranus
-			new Body(1.024e26, PLANET_GEOMETRY, new Vec2(0, 4.500e12)) //Neptune
+			new Body(5.68e26,  PLANET_GEOMETRY, new Vec2(0, 1.432e12)), //Saturn
+			new Body(8.68e25,  PLANET_GEOMETRY, new Vec2(0, 2.867e12)), //Uranus
+			new Body(1.02e26,  PLANET_GEOMETRY, new Vec2(0, 4.515e12)) //Neptune
 		];
 
-		bodies[1].v = new Vec2(-49700, 0); //Mercury
+		bodies[1].v = new Vec2(-47400, 0); //Mercury
 		bodies[2].v = new Vec2(-35000, 0); //Venus
 		bodies[3].v = new Vec2(-29800, 0); //Earth
 		bodies[4].v = new Vec2(-24100, 0); //Mars
@@ -35,13 +36,13 @@ class SolarSystemSimulation {
 
 		let characteristics: SolarSystemPlanetCharacteristics[] = [
 			new SolarSystemPlanetCharacteristics(6.957e8, "#f59f00"), //Sun
-			new SolarSystemPlanetCharacteristics(2439700, "#adb5bd"), //Mercury
-			new SolarSystemPlanetCharacteristics(6051800, "#ffc078"), //Venus
-			new SolarSystemPlanetCharacteristics(6371000, "#1864ab"), //Earth
-			new SolarSystemPlanetCharacteristics(3396200, "#d9480f"), //Mars
+			new SolarSystemPlanetCharacteristics(2439500, "#adb5bd"), //Mercury
+			new SolarSystemPlanetCharacteristics(6052000, "#ffc078"), //Venus
+			new SolarSystemPlanetCharacteristics(6378000, "#1864ab"), //Earth
+			new SolarSystemPlanetCharacteristics(3396000, "#d9480f"), //Mars
 			new SolarSystemPlanetCharacteristics(71492000, "#e67700"), //Jupiter
 			new SolarSystemPlanetCharacteristics(60268000, "#ffc078"), //Saturn
-			new SolarSystemPlanetCharacteristics(24973000, "#74c0fc"), //Uranus
+			new SolarSystemPlanetCharacteristics(25559000, "#74c0fc"), //Uranus
 			new SolarSystemPlanetCharacteristics(24764000, "#1864ab"), //Neptune
 		];
 
